76d704974af9a6f6f622a2a06836e55662219466
Makaveli98 <54841301+Makaveli98@users.noreply.github.com>
2019-09-09 09:59:33 +0200

    H1-4

---
+++ src/Troublesome96/H01.java
@@ -0,0 +1,6 @@
+package Troublesome96;
+
+public class H01 {public static void main(String[] args) {
+    System.out.println("Dit is mijn eerste java klasse!");
+}
+}
---
+++ src/Troublesome96/H02part2.java
@@ -0,0 +1,16 @@
+package Troublesome96;
+
+import java.awt.*;
+import java.applet.*;
+
+public class H02part2 extends Applet {
+
+    public void init() {setBackground(Color.black);}
+
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("Makaveli", 35, 50 );
+        g.setColor(Color.red);
+        g.drawString("TheDon", 25, 40 );
+    }
+}
