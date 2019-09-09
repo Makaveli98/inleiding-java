c3a661fd2203be37b2158de346fdc433785387a7
Makaveli98 <54841301+Makaveli98@users.noreply.github.com>
2019-09-09 12:51:20 +0200

    H1-H5 (h5 niet aF)

---
+++ src/Troublesome96/H02.java
@@ -0,0 +1,14 @@
+package Troublesome96;
+
+import java.awt.*;
+import java.applet.*;
+
+public class H02 extends Applet {
+
+    public void init() {setBackground(Color.black);}
+
+    public void paint(Graphics g) {
+        g.setColor(Color.red);
+        g.drawString("MakaveliTheDon", 35, 50 );
+    }
+}
---
+++ src/Troublesome96/H05part2.java
@@ -0,0 +1,16 @@
+package Troublesome96;
+
+import java.awt.*;
+import java.applet.*;
+
+public class H05part2 extends Applet{
+
+    int hoogteShen;
+    int hoogteZe;
+    int hoogteAkali;
+
+
+
+
+
+}
